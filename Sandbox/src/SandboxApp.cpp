007b514e420fd9a4761098c82f9a51c464fe4c30
MarkovicLuka95 <lukavrl@gmail.com>
2020-03-25 20:33:19 +0100

    log added

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,6 +1,13 @@
 #include "Test.h"
+#include "spdlog/spdlog.h"
 
 int main()
 {
+	spdlog::info("Welcome to spdlog!");
+	spdlog::error("Some error message with arg: {}", 1);
+	spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+	spdlog::warn("Easy padding in numbers like {:08d}", 12);
+	spdlog::set_level(spdlog::level::debug);
+	spdlog::debug("This message should be displayed..");
 	Rampart::Print();
 }
